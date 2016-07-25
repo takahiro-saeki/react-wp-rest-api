722f43b84b337b15769f9ede0478987b9b2cd934
takahiro-saeki <hiroooon69@gmail.com>
2016-07-26 01:35:55 +0900

    prepare to super agent. and basic sample.

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 import Main from './container/Main';
 import STYLE from '../css/style.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 render(
   <Main />,
   document.getElementById('app')
--- assets/js/container/Main.jsx
+++ assets/js/container/Main.jsx
@@ -2,8 +2,27 @@ import React, {Component} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import {Mui} from '../data/mui';
+import request from 'superagent';
+
 
 export default class Main extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  receive() {
+    request
+    .get('http://mohu-para.com/wp-json/wp/v2/posts')
+    .end((err, res) => {
+      if(err) {
+        console.log(err)
+      } else {
+        console.log(res)
+      }
+    });
+
+  }
+
   render() {
     return (
       <MuiThemeProvider muiTheme={Mui}>
@@ -12,6 +31,11 @@ export default class Main extends Component {
             title="Title"
             iconClassNameRight="muidocs-icon-navigation-expand-more"
           />
+        <p style={{
+            background: '#CCC',
+            padding: '1rem'
+          }}
+          onClick={this.receive}>サンプル</p>
         </main>
       </MuiThemeProvider>
     )
